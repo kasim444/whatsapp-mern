b633e2100d2f6508f0302414fe281c42e12458ce
Kasim <kasim1411@gmail.com>
2020-12-18 01:19:37 +0300

    remove unused firebase instance

--- front-end/src/firebase/index.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyBC_zLejGwJVbj5zenpyabtp5G6Tw4ZNg0',
-  authDomain: 'whatsapp-mern-71a2e.firebaseapp.com',
-  projectId: 'whatsapp-mern-71a2e',
-  storageBucket: 'whatsapp-mern-71a2e.appspot.com',
-  messagingSenderId: '381270106368',
-  appId: '1:381270106368:web:b912e6471db783ced5d3e5',
-}
